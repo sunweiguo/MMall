9fc0f64cf89051e89ad9d1a69895e975504cbb73
sunweiguo <317758022@qq.com>
2019-01-08 11:07:31 +0800

    readme

--- snailmall-order-service/src/main/java/com/njupt/swg/service/OrderServiceImpl.java
+++ snailmall-order-service/src/main/java/com/njupt/swg/service/OrderServiceImpl.java
@@ -533,52 +533,50 @@ public class OrderServiceImpl implements IOrderService {
         }
         resultMap.put("orderNo",String.valueOf(order.getOrderNo()));
 
-        //支付宝下单
-        // (必填) 商户网站订单系统中唯一订单号，64个字符以内，只能包含字母、数字、下划线，
-        // 需保证商户系统端不能重复，建议通过数据库sequence生成，
+        //1.获取订单号
         String outTradeNo = order.getOrderNo().toString();
 
 
-        // (必填) 订单标题，粗略描述用户的支付目的。如“xxx品牌xxx门店当面付扫码消费”
+        //2.订单标题，粗略描述用户的支付目的。如“xxx品牌xxx门店当面付扫码消费”
         String subject = new StringBuilder().append("快乐蜗牛商城扫码支付,订单号:").append(outTradeNo).toString();
 
 
-        // (必填) 订单总金额，单位为元，不能超过1亿元
+        // 3.(必填) 订单总金额，单位为元，不能超过1亿元
         // 如果同时传入了【打折金额】,【不可打折金额】,【订单总金额】三者,则必须满足如下条件:【订单总金额】=【打折金额】+【不可打折金额】
         String totalAmount = order.getPayment().toString();
 
 
-        // (可选) 订单不可打折金额，可以配合商家平台配置折扣活动，如果酒水不参与打折，则将对应金额填写至此字段
+        // 4.(可选) 订单不可打折金额，可以配合商家平台配置折扣活动，如果酒水不参与打折，则将对应金额填写至此字段
         // 如果该值未传入,但传入了【订单总金额】,【打折金额】,则该值默认为【订单总金额】-【打折金额】
         String undiscountableAmount = "0";
 
 
 
-        // 卖家支付宝账号ID，用于支持一个签约账号下支持打款到不同的收款账号，(打款到sellerId对应的支付宝账号)
+        // 5.卖家支付宝账号ID，用于支持一个签约账号下支持打款到不同的收款账号，(打款到sellerId对应的支付宝账号)
         // 如果该字段为空，则默认为与支付宝签约的商户的PID，也就是appid对应的PID
         String sellerId = "";
 
-        // 订单描述，可以对交易或商品进行一个详细地描述，比如填写"购买商品2件共15.00元"
+        // 6.订单描述，可以对交易或商品进行一个详细地描述，比如填写"购买商品2件共15.00元"
         String body = new StringBuilder().append("订单").append(outTradeNo).append("购买商品共").append(totalAmount).append("元").toString();
 
 
-        // 商户操作员编号，添加此参数可以为商户操作员做销售统计
+        // 7.商户操作员编号，添加此参数可以为商户操作员做销售统计
         String operatorId = "test_operator_id";
 
-        // (必填) 商户门店编号，通过门店号和商家后台可以配置精准到门店的折扣信息，详询支付宝技术支持
+        // 8.(必填) 商户门店编号，通过门店号和商家后台可以配置精准到门店的折扣信息，详询支付宝技术支持
         String storeId = "test_store_id";
 
-        // 业务扩展参数，目前可添加由支付宝分配的系统商编号(通过setSysServiceProviderId方法)，详情请咨询支付宝技术支持
+        // 9.业务扩展参数，目前可添加由支付宝分配的系统商编号(通过setSysServiceProviderId方法)，详情请咨询支付宝技术支持
         ExtendParams extendParams = new ExtendParams();
         extendParams.setSysServiceProviderId("2088100200300400500");
 
 
 
 
-        // 支付超时，定义为120分钟
+        // 10.支付超时，定义为120分钟
         String timeoutExpress = "120m";
 
-        // 商品明细列表，需填写购买商品详细信息，
+        // 11.商品明细列表，需填写购买商品详细信息，
         List<GoodsDetail> goodsDetailList = new ArrayList<GoodsDetail>();
 
         List<OrderItem> orderItemList = orderItemMapper.getByOrderNoUserId(orderNo,userId);
@@ -589,7 +587,7 @@ public class OrderServiceImpl implements IOrderService {
             goodsDetailList.add(goods);
         }
 
-        // 创建扫码支付请求builder，设置请求参数
+        // 12.创建扫码支付请求builder，设置请求参数
         AlipayTradePrecreateRequestBuilder builder = new AlipayTradePrecreateRequestBuilder()
                 .setSubject(subject).setTotalAmount(totalAmount).setOutTradeNo(outTradeNo)
                 .setUndiscountableAmount(undiscountableAmount).setSellerId(sellerId).setBody(body)
@@ -598,7 +596,7 @@ public class OrderServiceImpl implements IOrderService {
                 .setNotifyUrl(PropertiesUtil.getProperty("alipay.callback.url"))//支付宝服务器主动通知商户服务器里指定的页面http路径,根据需要设置
                 .setGoodsDetailList(goodsDetailList);
 
-
+        // 13.创建扫码支付请求成功后，支付宝返回一串二维码串
         AlipayF2FPrecreateResult result = tradeService.tradePrecreate(builder);
         switch (result.getTradeStatus()) {
             case SUCCESS:
